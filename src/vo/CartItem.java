1c30e7311ecf524fda14049eab1d037925a3c169
ljjs0121 <ljjs1215@gmail.com>
2023-05-08 00:34:35 +0900

    Value Object 패키지

---
+++ src/vo/CartItem.java
@@ -0,0 +1,35 @@
+package vo;
+
+public class CartItem {
+
+	private int userNo;
+	private int productNo;
+	private int amount;
+	
+	public CartItem () {}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public int getProductNo() {
+		return productNo;
+	}
+
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+
+	public int getamount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+}
